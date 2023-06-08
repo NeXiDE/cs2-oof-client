a7b16b80cb44cb3c056d5b07103b7bd74a70602f
NeXi2k <FreeUA3@outlook.de>
2023-06-08 20:26:57 +0200

    Reverted hotfix attempt

--- VPK/pak02/scripts/vscripts/patches/maxplayerCheck.lua
+++ VPK/pak02/scripts/vscripts/patches/maxplayerCheck.lua
@@ -22,24 +22,9 @@ function playerLeft(event)
     --print("Player left, Player Count: " .. playerLimit)
 end
 
-function newMapFix(event)
-    if(ScriptGetRoundsPlayed() == 0) then
-        if(ScriptIsWarmupPeriod() == false) then
-            print("Player spawned in first round, Player Count: " .. playerLimit)
-            playerLimit = playerlimit + 1
-        elseif (ScriptIsWarmupPeriod() == true) then
-            print("New map started. Not counting players yet")
-            playerLimit = 0
-        end
-    else
-        print("We are on round: " .. ScriptGetRoundsPlayed())
-    end
-end
-
 function Activate()
     ListenToGameEvent("player_connect", playerJoined, nil)
     ListenToGameEvent("player_disconnect", playerLeft, nil)
-    ListenToGameEvent("player_spawn", newMapFix, nil)
 end
 
 Activate()
