d8bbd9e61fde39a30ee09dec5e94028c70412bce
NeXi2k <FreeUA3@outlook.de>
2023-06-12 04:03:35 +0200

    Optimized and fixed alot of batch file issues

--- Mod Loading Files/Start Server.bat
+++ Mod Loading Files/Start Server.bat
@@ -1,9 +1,8 @@
 @echo off
-cd game
-copy /y "gameinfo_modded.gi" "csgo/gameinfo.gi"
-cd ../
-cls
+goto reload
+:reload
 echo Starting Counter-Strike: 2 Server...
-start "" "game\bin\win64\cs2.exe" -dedicated -console -condebug +mapgroup mg_cs2 +map de_dust2
-timeout /t 5 /nobreak
-exit
+start /wait game\bin\win64\cs2.exe -dedicated -console -condebug +mapgroup mg_cs2 +map de_mirage +serverconfigfile serverconfig.cfg
+echo Server Has Crashed %time% %date% >> log.txt
+timeout 3
+goto reload
--- Mod Loading Files/Workshop Tools - RAYTRACING.bat
+++ Mod Loading Files/Workshop Tools - RAYTRACING.bat
@@ -1,9 +1,5 @@
 @echo off
-cd game
-copy /y "gameinfo_original.gi" "csgo/gameinfo.gi"
-cd ../
-cls
 echo Starting CS:2 Workshop Tools...
-start "" "game\bin\win64\csgocfg.exe" -gpuraytracing
+start "" "game\bin\win64\csgocfg.exe"
 timeout /t 5 /nobreak
 exit
