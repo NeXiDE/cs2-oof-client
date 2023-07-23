5d95e2edaa4a4be62a34442c4802df5a58367368
Macdie <47941737+Krapisa@users.noreply.github.com>
2023-07-23 15:34:55 +0200

    Update Workshop Tools - RAYTRACING.bat
    Added launch argument to enable Raytracing on compatible GPUs

--- Mod Loading Files/Workshop Tools - RAYTRACING.bat
+++ Mod Loading Files/Workshop Tools - RAYTRACING.bat
@@ -1,6 +1,6 @@
 @echo off
 color 6
 echo Starting CS:2 Workshop Tools - Raytracing Mode...
-start "" "game\bin\win64\csgocfg.exe"
+start "" "game\bin\win64\csgocfg.exe" -gpuraytracing
 timeout /t 5 /nobreak
 exit
